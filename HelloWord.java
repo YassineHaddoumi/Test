4822a2964ba7227a6f71fb1a4e36e66fa8e80842
YassineHaddoumi <yhaddoumi@cerebotani.it>
2023-10-28 08:14:26 +0200

    FunzionalitàSomma

--- HelloWord.java
+++ HelloWord.java
@@ -1,5 +1,7 @@
 class HelloWord{
 	public static void main(String args[]){
-		System.out.println("Ciao Mondo");
+		int num1 = 5;
+		int num2 = 3;
+		System.out.println(num1 + "+" + num2 + "=" + (num1+num2));
 	}
 }
